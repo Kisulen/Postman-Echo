106e25a790a08c48a291a6cbccfe5e855701eac8
Maria Ustyuzhanina <elkaran@gmail.com>
2023-02-06 18:38:33 +0300

    Tests prepared

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,48 @@
+import jdk.jfr.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void someDataTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some value") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+
+    @Test
+    void phoneNumber() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body ("8999-999-99-99")
+                .when()
+                .post ("/post")
+                .then()
+                .body ("data", equalTo("8999-999-99-99"));
+    }
+
+    @Test
+    void fullName() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body ("Котикова Мария Котовна")
+                .when()
+                .post ("/post")
+                .then()
+                .body ("data", equalTo("Котикова Мария Котовна"));
+    }
+
+    }
+
